8118e23a1d0433746134236d545abe299dbdd870
Florian Chis <florian.chis@threepillarglobal.com>
2013-01-16 11:48:45 +0200

    fix imports

--- spring-data-simpledb-integration-tests/src/main/java/org/springframework/data/simpledb/sample/simpledb/domain/SimpleDbUser.java
+++ spring-data-simpledb-integration-tests/src/main/java/org/springframework/data/simpledb/sample/simpledb/domain/SimpleDbUser.java
@@ -1,12 +1,9 @@
 package org.springframework.data.simpledb.sample.simpledb.domain;
 
-import org.springframework.data.jpa.domain.AbstractPersistable;
 import org.springframework.data.simpledb.annotation.Attributes;
 import org.springframework.data.simpledb.annotation.Domain;
 import org.springframework.data.simpledb.annotation.ItemName;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
 import java.util.Map;
 
 @Domain(value = "Gigi")
