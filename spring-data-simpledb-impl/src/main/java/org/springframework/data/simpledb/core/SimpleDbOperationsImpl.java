c6f816baecce47c48fc6c6035134c6a5b309f943
Florian Chis <florian.chis@threepillarglobal.com>
2013-01-21 11:38:02 +0200

    add & update operations

--- spring-data-simpledb-impl/src/main/java/org/springframework/data/simpledb/core/SimpleDbOperationsImpl.java
+++ spring-data-simpledb-impl/src/main/java/org/springframework/data/simpledb/core/SimpleDbOperationsImpl.java
@@ -3,12 +3,16 @@ package org.springframework.data.simpledb.core;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.services.simpledb.AmazonSimpleDB;
 import com.amazonaws.services.simpledb.AmazonSimpleDBClient;
+import com.amazonaws.services.simpledb.model.PutAttributesRequest;
+import com.amazonaws.services.simpledb.model.ReplaceableAttribute;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.simpledb.repository.support.entityinformation.SimpleDbEntityInformation;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.springframework.util.Assert;
 
 /**
@@ -61,20 +65,28 @@ public class SimpleDbOperationsImpl<T, ID extends Serializable> implements Simpl
     }
 
     @Override
-    public void delete(SimpleDbEntity sdbEntity) {
-        logOperation("Delete", sdbEntity);
-    }
-
-    @Override
     public Object addItem(SimpleDbEntity entity) {
         logOperation("Add  ", entity);
+        Assert.notNull(entity.getDomain(), "Domain name should not be null");
+        Assert.notNull(entity.getItemName(), "Item name should not be null");
+        sdb.putAttributes(new PutAttributesRequest(entity.getDomain(), entity.getItemName(), toReplaceableAttributeList(entity.getAttributes(), false)));
         return entity.getItem();
     }
 
     @Override
     public Object updateItem(SimpleDbEntity entity) {
         logOperation("Update", entity);
+        Assert.notNull(entity.getDomain(), "Domain name should not be null");
+        Assert.notNull(entity.getItemName(), "Item name should not be null");
+        sdb.putAttributes(new PutAttributesRequest(entity.getDomain(), entity.getItemName(), toReplaceableAttributeList(entity.getAttributes(), true)));
         return entity.getItem();
+    }
+
+    @Override
+    public void delete(SimpleDbEntity entity) {
+        logOperation("Delete", entity);
+        Assert.notNull(entity.getDomain(), "Domain name should not be null");
+        Assert.notNull(entity.getItemName(), "Item name should not be null");
     }
 
     @Override
@@ -94,6 +106,14 @@ public class SimpleDbOperationsImpl<T, ID extends Serializable> implements Simpl
     public List findAll(SimpleDbEntityInformation entityInformation, Iterable ids) {
         LOGGER.info("Find All Domain \"{}\"\"", entityInformation.getDomain());
         return null;
+    }
+
+    private List<ReplaceableAttribute> toReplaceableAttributeList(Map<String, String> attributes, boolean replace) {
+        List<ReplaceableAttribute> result = new ArrayList<>();
+        for (String key : attributes.keySet()) {
+            result.add(new ReplaceableAttribute(key, attributes.get(key), replace));
+        }
+        return result;
     }
 
     private void logOperation(String operation, SimpleDbEntity<T, ID> entity) {
