7128aa4b3a7b516e92c2e92d596d51729a884cf4
Florian Chis <florian.chis@threepillarglobal.com>
2013-01-21 11:00:49 +0200

    initial DDL impl

--- spring-data-simpledb-impl/src/main/java/org/springframework/data/simpledb/core/SimpleDBDomainOperations.java
+++ spring-data-simpledb-impl/src/main/java/org/springframework/data/simpledb/core/SimpleDBDomainOperations.java
@@ -1,27 +1,38 @@
 package org.springframework.data.simpledb.core;
 
+import com.amazonaws.services.simpledb.AmazonSimpleDB;
+import com.amazonaws.services.simpledb.model.CreateDomainRequest;
+import com.amazonaws.services.simpledb.model.DeleteDomainRequest;
+import com.amazonaws.services.simpledb.model.ListDomainsRequest;
 import java.util.List;
-
 
 public class SimpleDBDomainOperations {
 
+    //TODO
+    private AmazonSimpleDB sdb;
 
-    public void dropDomain(String domainName){
-
+    public void dropDomain(String domainName) {
+        sdb.deleteDomain(new DeleteDomainRequest(domainName));
     }
 
-    public List<String> fetchDomainNames(){
-
-
-           return null;
+    public List<String> fetchDomainNames() {
+        return sdb.listDomains().getDomainNames();
     }
 
-    public void createDomain(){
-
+    public void createDomain(String domainName) {
+        sdb.createDomain(new CreateDomainRequest(domainName));
     }
 
-    public void createDomainsIfNotExist(){
-
+    public void createDomainsIfNotExist(String domainName) {
+        boolean found = false;
+        for(String domain : fetchDomainNames()) {
+            if(domain.equals(domainName)) {
+                found = true;
+                break;
+            }
+        }
+        if(!found) {
+            sdb.createDomain(new CreateDomainRequest(domainName));
+        }
     }
-    
 }
