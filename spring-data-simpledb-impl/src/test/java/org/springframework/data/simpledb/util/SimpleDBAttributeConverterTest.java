0639e1793ee15729123d5833c5ae00b3186d5949
karolyszanto <szanto.karoly@gmail.com>
2013-01-28 11:48:43 +0200

    Individual tests for convert/decode primitive types

--- spring-data-simpledb-impl/src/test/java/org/springframework/data/simpledb/util/SimpleDBAttributeConverterTest.java
+++ spring-data-simpledb-impl/src/test/java/org/springframework/data/simpledb/util/SimpleDBAttributeConverterTest.java
@@ -3,8 +3,10 @@ package org.springframework.data.simpledb.util;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -142,5 +144,109 @@ public class SimpleDBAttributeConverterTest {
 		assertNotNull(convertedValue);
 		assertTrue(((Date)SimpleDBAttributeConverter.toDomainFieldPrimitive(convertedValue, Date.class)).equals(domainEntity.dateField));
 	}
+	
+	/* ********************* individually test converter methods ********************** */
+    @Test
+    public void test_padding_long_value(){
+        long longValue = 12345L;
+        String encoded = SimpleDBAttributeConverter.toSimpleDBAttributeValue(longValue);
+        
+        assertEquals("09223372036854788153", encoded);
+    }
+    
+    @Test
+    public void test_padding_int_value(){
+        int intValue = 1;
+        String encoded = SimpleDBAttributeConverter.toSimpleDBAttributeValue(intValue);
+        
+        assertEquals("09223372036854775809", encoded);
+    }
+    
+    @Test
+    public void test_encode_int_value() {
+    	int x = -390293;
+        String encoded;
+        BigDecimal bdx;
+        
+        bdx = new BigDecimal(x);
+        encoded = AmazonSimpleDBUtil.encodeRealNumberRange(bdx, 11, new BigDecimal(Integer.MIN_VALUE).negate());
+        assertEquals("02147093355", encoded);
+    }
+    
+    @Test
+    public void test_decode_int_value() {
+    	String encodedInt = "02147093355";
+    	BigDecimal bgdecoded = AmazonSimpleDBUtil.decodeRealNumberRange(encodedInt, new BigDecimal(Integer.MIN_VALUE).negate());
+        assertEquals(-390293, bgdecoded.intValue());
+    }
+
+    @Test
+    public void test_encode_and_decode_long() {
+        long x;
+
+        x = 209323021234234498L;
+        encodeAndDecode(x);
+
+        x = Long.MAX_VALUE;
+        encodeAndDecode(x);
+
+        x = Long.MIN_VALUE;
+        encodeAndDecode(x);
+    }
+
+    @Test
+    public void test_encode_and_decode_double() {
+        double x;
+
+        x = 500.0;
+        encodeAndDecode(x);
+
+        x = 20932304234498.039;
+        encodeAndDecode(x);
+
+        x = Double.MAX_VALUE;
+        encodeAndDecode(x);
+
+        /* Does not work
+        x = Double.MIN_VALUE;
+        encodeAndDecode(x); */
+
+        x = -209323021234234498.902938903849082349;
+        encodeAndDecode(x);
+    }
+
+    @Test
+    public void test_encode_and_decode_big_decimal() {
+        BigDecimal x;
+
+        x = new BigDecimal("20932304234498.039");
+        encodeAndDecode(x);
+
+        x = new BigDecimal("-209323021234234498.902938903849082349");
+        encodeAndDecode(x);
+    }
+
+    private void encodeAndDecode(double x) {
+        encodeAndDecode(new BigDecimal(x));
+    }
+
+    private void encodeAndDecode(long x) {
+        String encoded;
+        BigDecimal bgdecoded;
+        BigDecimal bdx = new BigDecimal(x);
+
+        encoded = AmazonSimpleDBUtil.encodeRealNumberRange(bdx, 20, new BigDecimal(Long.MIN_VALUE).negate());
+        bgdecoded = AmazonSimpleDBUtil.decodeRealNumberRange(encoded, new BigDecimal(Long.MIN_VALUE).negate());
+
+        assertEquals(bdx, bgdecoded);
+    }
+
+    private void encodeAndDecode(BigDecimal bdx) {
+        String encoded;
+        BigDecimal bgdecoded;
+        encoded = AmazonSimpleDBUtil.encodeRealNumberRange(bdx, 20, 20, new BigDecimal(Long.MIN_VALUE).negate());
+        bgdecoded = AmazonSimpleDBUtil.decodeRealNumberRange(encoded, 20, new BigDecimal(Long.MIN_VALUE).negate());
+        assertTrue(bdx.compareTo(bgdecoded) == 0);
+    }
 
 }
