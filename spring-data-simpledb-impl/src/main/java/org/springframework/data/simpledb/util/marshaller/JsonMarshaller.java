3fef1b071229611a070031eb066c729c1e318498
aposorovaschi <alin.posorovaschi@3pillarglobal.com>
2013-01-29 15:13:50 +0200

    Add the JSON marshaller from IBIS to the project.

---
+++ spring-data-simpledb-impl/src/main/java/org/springframework/data/simpledb/util/marshaller/JsonMarshaller.java
@@ -0,0 +1,118 @@
+package org.springframework.data.simpledb.util.marshaller;
+
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.mrbean.MrBeanModule;
+import org.codehaus.jackson.type.TypeReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.mapping.model.MappingException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+
+public class
+        JsonMarshaller implements Marshaller {
+
+    private static final Logger log = LoggerFactory.getLogger(JsonMarshaller.class);
+
+    private ObjectMapper mapper;
+
+    public JsonMarshaller() {
+        JsonFactory factory = new JsonFactory();
+        mapper = new ObjectMapper(factory);
+        JsonUnknownPropertyHandler jsonUnknownPropertyHandler = new JsonUnknownPropertyHandler();
+        mapper.getDeserializationConfig().addHandler(jsonUnknownPropertyHandler);
+        mapper.registerModule(new MrBeanModule());
+    }
+
+    @Override
+    public <T> T unmarshal(String input, Class<T> clazz) {
+
+        if (input == null) {
+            log.warn("Null input given to unmarshal, will return null.");
+            return null;
+        }
+
+        T unmarshalledObject;
+        try {
+            unmarshalledObject = mapper.readValue(input, clazz);
+        } catch (JsonParseException e) {
+            //in case of error, if the required class is string, just return the original input
+            if (clazz.equals(String.class)) {
+                return (T) input;
+            }
+            throw new MappingException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+
+        return unmarshalledObject;
+    }
+
+    @Override
+    public <T> T unmarshal(String input, TypeReference<T> typeReference) {
+
+        if (input == null) {
+            log.warn("Null input given to unmarshal, will return null.");
+            return null;
+        }
+
+        T unmarshalledObject;
+        try {
+            unmarshalledObject = mapper.<T>readValue(input, typeReference);
+        } catch (Exception e) {
+            throw new MappingException(e.getMessage(), e);
+        }
+
+        return unmarshalledObject;
+    }
+
+    @Override
+    public <T> T unmarshal(InputStream input, Class<T> clazz) {
+
+        if (input == null) {
+            log.warn("Null input given to unmarshal, will return null.");
+            return null;
+        }
+
+        BufferedReader jsonReader = new BufferedReader(new InputStreamReader(input));
+        StringBuilder jsonOut = new StringBuilder();
+        try {
+            String line = jsonReader.readLine();
+            while (line != null) {
+                jsonOut.append(line);
+                line = jsonReader.readLine();
+            }
+            String json = jsonOut.toString();
+            return unmarshal(json, clazz);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+
+    }
+
+    @Override
+    public <T> String marshal(T input) {
+
+        if (input == null) {
+            log.warn("Null input given to marshal, will return null.");
+            return null;
+        }
+
+        String marshalledObject;
+        try {
+            marshalledObject = mapper.writeValueAsString(input);
+        } catch (Exception e) {
+            throw new MappingException(e.getMessage(), e);
+        }
+
+        return marshalledObject;
+    }
+
+}
---
+++ spring-data-simpledb-impl/src/main/java/org/springframework/data/simpledb/util/marshaller/Marshaller.java
@@ -0,0 +1,43 @@
+package org.springframework.data.simpledb.util.marshaller;
+
+import org.codehaus.jackson.type.TypeReference;
+
+import java.io.InputStream;
+
+/**
+ * Provides a common interface for marshalling/unmarshalling POJOs
+ */
+public interface Marshaller {
+
+    /**
+     * Populates an object from a string representation.
+     *
+     * @param clazz the class of the object to populate.
+     * @param input the string to unmarshall.
+     * @return An object populated from unmarshalling the string representation.
+     */
+    <T extends Object> T unmarshal(String input, Class<T> clazz);
+
+
+    /**
+     * Populates an object from input stream providing a representation for the destination object.
+     *
+     * @param clazz the class of the object to populate.
+     * @param input the input stream to take object's representation from.
+     * @return An instance of the object populated from the representation.
+     */
+    <T extends Object> T unmarshal(InputStream input, Class<T> clazz);
+
+
+    <T extends Object> T unmarshal(String input, TypeReference<T> clazz);
+
+    /**
+     * Creates a string representation of an object.
+     *
+     * @param input the object to marshall.
+     * @return A string representation of the object.
+     */
+    <T extends Object> String marshal(T input);
+
+
+}
