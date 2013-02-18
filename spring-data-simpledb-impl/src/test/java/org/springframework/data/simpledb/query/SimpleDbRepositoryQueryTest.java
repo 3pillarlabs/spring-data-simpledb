216e4f1dcbdd7292abe4e71940fc3798e7c786d1
mgrozea <maria.grozea@threepillarglobal.com>
2013-02-18 17:14:42 +0200

    fix test

--- spring-data-simpledb-impl/src/test/java/org/springframework/data/simpledb/query/SimpleDbRepositoryQueryTest.java
+++ spring-data-simpledb-impl/src/test/java/org/springframework/data/simpledb/query/SimpleDbRepositoryQueryTest.java
@@ -12,7 +12,7 @@ public class SimpleDbRepositoryQueryTest {
     @Test (expected = IllegalArgumentException.class)
     public void assertNotHavingNestedQueryParameters_should_fail_for_nested_attributes() {
         SimpleDbQueryMethod method = Mockito.mock(SimpleDbQueryMethod.class);
-        Mockito.when(method.getDomainClass()).thenReturn((Class) SampleEntity.class);
+        Mockito.when(method.getDomainClazz()).thenReturn((Class) SampleEntity.class);
 
         SimpleDbRepositoryQuery repositoryQuery = new SimpleDbRepositoryQuery(method, null);
         repositoryQuery.assertNotHavingNestedQueryParameters("select sampleNestedAttribute from SampleEntity");
