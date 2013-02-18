62d985f6286b73606efcef14109f8fa272f06ba7
mgrozea <maria.grozea@threepillarglobal.com>
2013-02-18 11:12:52 +0200

    added javadoc

--- spring-data-simpledb-impl/src/main/java/org/springframework/data/simpledb/query/executions/MultipleResultType.java
+++ spring-data-simpledb-impl/src/main/java/org/springframework/data/simpledb/query/executions/MultipleResultType.java
@@ -1,7 +1,14 @@
 package org.springframework.data.simpledb.query.executions;
 
 /**
- * Enum for different type of queries returning a collection result.
+ * The following multiple result types can be requested: <br/>
+ * <ul>
+ *     <li>COLLECTION_OF_DOMAIN_ENTITIES - List&lt;Entity&gt; <br/> as returned type for query  <code> select * from entity</code> </li>
+ *     <li>LIST_BASED_REPRESENTATION - List&lt; List &lt; Object &gt; &gt;</li> <br/> as returned type for query <code> select aField, bField from entity</code>
+ *     <li>FIELD_OF_TYPE_COLLECTION - Collection &lt; ? &gt; </li> <br/> as returned type for query <code> select collectionField from entity where itemName()="1"</code>
+ *     <li>LIST_OF_FIELDS - List &lt; ? &gt; </li> <br/> as returned type for query <code> select aField from entity</code>
+ *     <li>SET_OF_FIELDS - Set &lt; ? &gt; </li> <br/> as returned type for query <code> select aField from entity</code>
+ * </ul>
  */
 public enum MultipleResultType {
     COLLECTION_OF_DOMAIN_ENTITIES,
