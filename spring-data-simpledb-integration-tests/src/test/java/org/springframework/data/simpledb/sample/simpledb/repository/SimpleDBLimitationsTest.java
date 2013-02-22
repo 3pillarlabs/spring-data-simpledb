8b6e44ab375f5eecfb8ac40c53ebbc65f6aff5d0
cmester <ciprian.mester@threepillarglobal.com>
2013-02-22 13:52:40 +0200

    remove duplicated classˆ

--- spring-data-simpledb-integration-tests/src/test/java/org/springframework/data/simpledb/sample/simpledb/repository/SimpleDBLimitationsTest.java
+++ spring-data-simpledb-integration-tests/src/test/java/org/springframework/data/simpledb/sample/simpledb/repository/SimpleDBLimitationsTest.java
@@ -9,8 +9,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.simpledb.sample.simpledb.domain.SimpleDbUser;
-import org.springframework.data.simpledb.sample.simpledb.repository.util.RandomValueGenerator;
 import org.springframework.data.simpledb.sample.simpledb.repository.util.SimpleDbUserBuilder;
+import org.springframework.data.simpledb.util.RandomValueGenerator;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
