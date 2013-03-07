e10378612fc45ed392414f6d3efb3a5ba5245bef
karolyszanto <szanto.karoly@gmail.com>
2013-03-07 17:00:08 +0200

    Created dummy SimpleDBTemplate (with integration tests)

---
+++ spring-data-simpledb-impl/src/main/java/org/springframework/data/simpledb/core/ISimpleDBOperations.java
@@ -0,0 +1,34 @@
+package org.springframework.data.simpledb.core;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+/**
+ * Interface that specifies a basic set of SimpleDB operations.
+ */
+public interface ISimpleDBOperations {
+	
+	/**
+	 * The domain name used for the specified class by this template.
+	 * 
+	 * @param entityClass must not be {@literal null}.
+	 * @return the domain name for the specified class
+	 */
+	String getDomainName(Class<?> entityClass);
+
+	<T> T createOrUpdateItem(T entity);
+
+	void deleteItem(String domainName, String itemName);
+
+	<T, ID extends Serializable> T readItem(ID id, Class<T> entityClass, boolean consistentRead);
+
+	<T> long count(Class<T> entityClass, boolean consistentRead);
+
+	<T> List<T> find(Class<T> entityClass, String query, boolean consistentRead);
+
+	<T> Page<T> executePagedQuery(Class<T> entityClass, String query, Pageable pageable,
+			boolean consistentRead);
+}
---
+++ spring-data-simpledb-impl/src/main/java/org/springframework/data/simpledb/core/SimpleDBTemplate.java
@@ -0,0 +1,49 @@
+package org.springframework.data.simpledb.core;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+/**
+ * Primary implementation of {@link ISimpleDBOperations} 
+ */
+public class SimpleDBTemplate implements ISimpleDBOperations {
+
+	@Override
+	public String getDomainName(Class<?> entityClass) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public <T> T createOrUpdateItem(T entity) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void deleteItem(String domainName, String itemName) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public <T, ID extends Serializable> T readItem(ID id, Class<T> entityClass, boolean consistentRead) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public <T> long count(Class<T> entityClass, boolean consistentRead) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public <T> List<T> find(Class<T> entityClass, String query, boolean consistentRead) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public <T> Page<T> executePagedQuery(Class<T> entityClass, String query, Pageable pageable, boolean consistentRead) {
+		throw new UnsupportedOperationException();
+	}
+
+}
