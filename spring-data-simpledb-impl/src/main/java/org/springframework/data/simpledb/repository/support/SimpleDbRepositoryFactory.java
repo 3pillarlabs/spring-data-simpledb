aeef9cdf0c7d097eee686750468ec5a89a5264b9
karolyszanto <szanto.karoly@gmail.com>
2013-03-18 13:08:43 +0200

    Moved domain management into template

--- spring-data-simpledb-impl/src/main/java/org/springframework/data/simpledb/repository/support/SimpleDbRepositoryFactory.java
+++ spring-data-simpledb-impl/src/main/java/org/springframework/data/simpledb/repository/support/SimpleDbRepositoryFactory.java
@@ -1,19 +1,15 @@
 package org.springframework.data.simpledb.repository.support;
 
 import java.io.Serializable;
-import java.lang.reflect.Field;
-import java.util.List;
 
 import org.springframework.data.repository.core.RepositoryMetadata;
 import org.springframework.data.repository.core.support.RepositoryFactorySupport;
 import org.springframework.data.repository.query.QueryLookupStrategy;
 import org.springframework.data.simpledb.core.SimpleDbOperations;
 import org.springframework.data.simpledb.core.SimpleDbTemplate;
-import org.springframework.data.simpledb.core.domain.DomainManager;
 import org.springframework.data.simpledb.query.SimpleDbQueryLookupStrategy;
 import org.springframework.data.simpledb.repository.support.entityinformation.SimpleDbEntityInformation;
 import org.springframework.data.simpledb.repository.support.entityinformation.SimpleDbEntityInformationSupport;
-import org.springframework.data.simpledb.util.ReflectionUtils;
 
 /**
  * SimpleDB specific generic repository factory.
@@ -22,12 +18,8 @@ import org.springframework.data.simpledb.util.ReflectionUtils;
 public class SimpleDbRepositoryFactory extends RepositoryFactorySupport {
 
 	private SimpleDbOperations simpleDbOperations;
-	private DomainManager domainManager;
 
 	public SimpleDbRepositoryFactory(SimpleDbOperations simpleDbOperations) {
-		this.domainManager = new DomainManager(simpleDbOperations.getDB(), simpleDbOperations.getSimpleDb()
-				.getDomainManagementPolicy().toString());
-
 		this.simpleDbOperations = new SimpleDbTemplate(simpleDbOperations.getSimpleDb());
 
 	}
@@ -42,9 +34,6 @@ public class SimpleDbRepositoryFactory extends RepositoryFactorySupport {
 	@Override
 	protected Object getTargetRepository(RepositoryMetadata metadata) {
 		SimpleDbEntityInformation<?, Serializable> entityInformation = getEntityInformation(metadata.getDomainType());
-
-		// TODO: remove this when SimpleDBTemplate will provide functionality of managing domains
-		manageSimpleDbDomains(entityInformation);
 
 		SimpleDbRepositoryImpl<?, ?> repo = new SimpleDbRepositoryImpl(entityInformation, simpleDbOperations);
 
@@ -85,15 +74,4 @@ public class SimpleDbRepositoryFactory extends RepositoryFactorySupport {
 				simpleDbDomain);
 	}
 
-	private void manageSimpleDbDomains(SimpleDbEntityInformation<?, Serializable> entityInformation) {
-		List<Field> nestedReferences = ReflectionUtils.getReferenceAttributesList(entityInformation.getJavaType());
-
-		entityInformation.validateReferenceFields(nestedReferences);
-
-		for(Field eachNestedReference : nestedReferences) {
-			domainManager.manageDomain(simpleDbOperations.getDomainName(eachNestedReference.getType()));
-		}
-
-		domainManager.manageDomain(entityInformation.getDomain());
-	}
 }
