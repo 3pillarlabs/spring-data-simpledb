ea9ea0ad8fbc864c001d2ea67c55b52802ac6117
doom777 <doom777@gmail.com>
2014-05-23 16:20:50 +0300

    no need for special enum check

--- spring-data-simpledb-impl/src/main/java/org/springframework/data/simpledb/query/QueryUtils.java
+++ spring-data-simpledb-impl/src/main/java/org/springframework/data/simpledb/query/QueryUtils.java
@@ -83,7 +83,7 @@ public final class QueryUtils {
 			final Class<?> paramType = param.getType();
 
 			if (!(param.isSpecialParameter() || SupportedCoreTypes
-					.isSupported(paramType) || paramType.isEnum())) {
+					.isSupported(paramType))) {
 				throw (new IllegalArgumentException("Type " + paramType
 						+ " not supported as an annotated query parameter!"));
 			}
